950129088fc5827f6533a8072df78886a0b019ea
Alex <ph0t0nix@ph0t0nixs-MacBook-Pro.local>
2019-11-04 16:58:30 -0600

    fixing styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <div id='Home' className='headerstyle h-100 w-100'>
+        <div id='Home' className='headerstyle vh-100 w-100'>
             <article className='overlay h-100 w-100 flex flex-column flex-wrap justify-center'>
                 <div className='articletext pa4 white'>
                     <h1 className='f1 fw3 f-subheadline-l'>Hi, my name is Alex Monteil</h1>
