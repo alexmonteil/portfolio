8ae1b3c083b140fb3c2f554bb19dd8d2e8eaa227
Alex <ph0t0nix@ph0t0nixs-MacBook-Pro.local>
2019-11-04 16:46:48 -0600

    working on mobile responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ import './Header.css';
 const Header = () => {
     return (
         <div id='Home' className='headerstyle vh-100 w-100'>
-            <article className='overlay h-100 w-100 dt'>
-                <div className='articletext dtc tc white ph4 v-mid'>
+            <article className='overlay h-100 w-100 flex flex-column flex-wrap justify-center'>
+                <div className='articletext pa4 white'>
                     <h1 className='f1 fw3 f-subheadline-l'>Hi, my name is Alex Monteil</h1>
                     <br/>
                     <h1 className='f1 fw3 f-subheadline-l'>Welcome to my Portfolio !</h1>
