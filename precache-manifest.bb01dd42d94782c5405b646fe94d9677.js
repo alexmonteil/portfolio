0ccccdca8e3df067394b014369ee01489cb75b26
Alex <ph0t0nix@ph0t0nixs-MacBook-Pro.local>
2019-12-20 19:41:35 -0600

    Updates

--- precache-manifest.bb01dd42d94782c5405b646fe94d9677.js
+++ precache-manifest.bb01dd42d94782c5405b646fe94d9677.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "143919bc11f096ba91f45e05e2614a63",
+    "revision": "468b5d9fc68707c34fd3601fe179ffdf",
     "url": "/reactfolio/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/reactfolio/static/css/2.31dafeb2.chunk.css"
   },
   {
-    "revision": "588106dfab69a19d886a",
+    "revision": "3f111cfbd3ef33640586",
     "url": "/reactfolio/static/css/main.d2913d93.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/reactfolio/static/js/2.4e36794d.chunk.js"
   },
   {
-    "revision": "588106dfab69a19d886a",
-    "url": "/reactfolio/static/js/main.75c80acb.chunk.js"
+    "revision": "3f111cfbd3ef33640586",
+    "url": "/reactfolio/static/js/main.56b60f95.chunk.js"
   },
   {
     "revision": "22f791b4dac58735f89d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactfolio/precache-manifest.301787a4056585d8fcb189b6ca8a019f.js"
+  "/reactfolio/precache-manifest.bb01dd42d94782c5405b646fe94d9677.js"
 );
 
 self.addEventListener('message', (event) => {
