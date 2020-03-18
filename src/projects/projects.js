25a5503bdde3082b75ac0eaca5d2324842d4e03c
alexmonteil <alex.monteil@outlook.com>
2020-03-18 17:33:01 -0500

    fixing projects screenshots

--- src/projects/projects.js
+++ src/projects/projects.js
@@ -7,7 +7,7 @@ export const projects = [
     {
         id: "1",
         name: "Project 1",
-        thumbnail: "https://raw.githubusercontent.com/alexmonteil/projects-screenshots/master/binary-vision.png?token=ALMYFQTCLECJTWVBKBNEORK6OKFF4",
+        thumbnail: "https://raw.githubusercontent.com/alexmonteil/projects-screenshots/master/binary-vision.png",
         type: "react",
         description: "Project 1 description here",
         url: "#"
@@ -16,7 +16,7 @@ export const projects = [
     {
         id: "2",
         name: "Project 2",
-        thumbnail: "https://raw.githubusercontent.com/alexmonteil/projects-screenshots/master/galactic-directory.png?token=ALMYFQTKAFOGFX4BMLH4KHC6OKFHE",
+        thumbnail: "https://raw.githubusercontent.com/alexmonteil/projects-screenshots/master/galactic-directory.png",
         type: "react",
         description: "Project 2 description here",
         url: "#"
@@ -25,7 +25,7 @@ export const projects = [
     {
         id: "3",
         name: "Project 3",
-        thumbnail: "https://raw.githubusercontent.com/alexmonteil/projects-screenshots/master/git-lookup.png?token=ALMYFQQYXVZPCNRKEIS4LKK6OKFIG",
+        thumbnail: "https://raw.githubusercontent.com/alexmonteil/projects-screenshots/master/git-lookup.png",
         type: "javascript",
         description: "Project 3 description here",
         url: "#"
@@ -34,7 +34,7 @@ export const projects = [
     {
         id: "4",
         name: "Project 4",
-        thumbnail: "https://raw.githubusercontent.com/alexmonteil/projects-screenshots/master/task-master.png?token=ALMYFQWO7LCPGZWRGAYBQQ26OKFK2",
+        thumbnail: "https://raw.githubusercontent.com/alexmonteil/projects-screenshots/master/task-master.png",
         type: "javascript",
         description: "Project 4 description here",
         url: "#"
@@ -43,7 +43,7 @@ export const projects = [
     {
         id: "5",
         name: "Project 5",
-        thumbnail: "https://raw.githubusercontent.com/alexmonteil/projects-screenshots/master/loan-calculator.png?token=ALMYFQWIQQA6GKMFOCEXH2S6OKFJO",
+        thumbnail: "https://raw.githubusercontent.com/alexmonteil/projects-screenshots/master/loan-calculator.png",
         type: "bootstrap",
         description: "Project 5 description here",
         url: "#"
@@ -52,7 +52,7 @@ export const projects = [
     {
         id: "6",
         name: "Project 6",
-        thumbnail: "https://raw.githubusercontent.com/alexmonteil/projects-screenshots/master/reactfolio.png?token=ALMYFQVET34MNDWI4KNMHHC6OKFLW",
+        thumbnail: "https://raw.githubusercontent.com/alexmonteil/projects-screenshots/master/reactfolio.png",
         type: "react",
         description: "Project 6 description here",
         url: "#"
@@ -61,7 +61,7 @@ export const projects = [
     {
         id: "7",
         name: "Project 7",
-        thumbnail: "https://raw.githubusercontent.com/alexmonteil/projects-screenshots/master/background-generator.png?token=ALMYFQUB2N5D4DUUC7YQDDS6OKFEC",
+        thumbnail: "https://raw.githubusercontent.com/alexmonteil/projects-screenshots/master/background-generator.png",
         type: "css",
         description: "Project 7 description here",
         url: "#"
